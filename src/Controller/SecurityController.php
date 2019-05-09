f3d1a9e4c84e1389098e94a98383b197d3705427
Alexis Lepoutre <alepoutre@itroom.fr>
2019-05-09 16:41:27 +0200

    jwt - api platform

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SecurityController extends AbstractController
 {
@@ -16,6 +17,19 @@ class SecurityController extends AbstractController
         return $this->render('security/index.html.twig');
     }
     
+    /**
+* @Route(name="loginCheck",path="/api/login_check")
+*/
+public function loginCheck(Request $request): JsonResponse
+{
+$user = $this->getUser();
+
+return $this->json(array(
+'username' => $user->getUsername(),
+'roles' => $user->getRoles(),
+));
+}
+
     /**
     * @Route("/logout", name="security_logout")
     */
